c8961461e32a3b2e2c79e3aeb47a8f8189e1cb61
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-09-15 18:31:39 -0300

    update backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import cors from 'cors'
 import express from 'express'
+import { download } from './download.js';
 
 const app = express();
 app.use(cors());
 
 app.get('/summary/:id', (req,res) => {
-    res.send("ID do vídeo:" + req.params.id)
+    download(req.params.id)
+    res.json( {result: "Download realizado com sucesso!"} )
 })
 
 app.listen(3333, () => console.log('rodando garai'))
